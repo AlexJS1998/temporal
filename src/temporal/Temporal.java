97b843e6aa82a1a872e59b2df702e2333231a36a
Jorge Sánchez <Jorge Snchez@DESKTOP-JB9GA01>
2018-04-15 09:07:12 -0600

    Este es un comentario XD

---
+++ src/temporal/Temporal.java
@@ -0,0 +1,17 @@
+
+package temporal;
+
+/**
+ *Nombre clase:temporal
+ * Fecha:15/04/2018
+ * Version:1.0
+ * copyringht:
+ * @author Jorge Sánchez
+ */
+public class Temporal {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
